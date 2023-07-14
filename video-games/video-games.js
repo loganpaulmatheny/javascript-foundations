a6c3bf41be612ab2a9d9e564adb2e0cf315f4dcc
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-13 22:01:15 -0400

    complete video-game tests

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,40 @@
+function createPlayer(name, age, moves) {
+  var player = {
+    name: name,
+    age: age,
+    moveset: moves,
+  };
+  console.log(player.moveSet);
+  return player;
+}
 
+function createLevel(name, player) {
+  var level = {
+    name: name,
+    players: player,
+    coins: 0,
+    lives: 3,
+  };
+  return level;
+}
 
-
-module.exports = { };
+function findCoins(level, coins) {
+  level.coins = coins;
+  if (level.coins >= 100) {
+    level.lives += 1;
+  }
+  return level;
+}
+function defeatPlayer(level) {
+  level.lives -= 1;
+  if (level.lives === 0) {
+    return "GAME OVER";
+  }
+  return level;
+}
+module.exports = {
+  createPlayer,
+  createLevel,
+  findCoins,
+  defeatPlayer,
+};
