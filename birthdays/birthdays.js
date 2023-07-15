6f572dd1ef2ae362827eadf9fe5576893b399746
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-15 18:07:25 -0400

    complete birthday exercise

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,24 @@
+function createBirthday(name, month, day) {
+  var birthday = {
+    name: name,
+    month: month,
+    day: day,
+  };
+  return birthday;
+}
 
+function celebrateBirthday(personBirthday) {
+  var celebration = `Today is ${personBirthday.month}/${personBirthday.day}! Happy birthday, ${personBirthday.name}!`;
+  return celebration;
+}
 
-module.exports = {  };
+function countBirthdays(birthdayList, month) {
+  var count = 0;
+  for (let i = 0; i < birthdayList.length; i++) {
+    if (birthdayList[i].month === month) {
+      count += 1;
+    }
+  }
+  return count;
+}
+module.exports = { createBirthday, celebrateBirthday, countBirthdays };
