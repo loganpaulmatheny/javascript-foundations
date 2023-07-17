bafe0bdba2c4114620db687349eef55cfcd9640b
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-17 16:57:40 -0400

    Pass library tests

--- library/library.js
+++ library/library.js
@@ -1,3 +1,32 @@
+function issueCard(name, age) {
+  var card = {
+    name: name,
+    age: age,
+    numBooksCheckedOut: 0,
+    isChild: false,
+  };
+  if (age < 13) {
+    card.isChild = true;
+  }
+  return card;
+}
 
+function searchByAuthor(books, name) {
+  // iterate through books
+  // compare name to author property
+  // push that to found array
+  // return found
+  var found = [];
+  for (let i = 0; i < books.length; i++) {
+    if (books[i].author === name) {
+      found.push(books[i]);
+    }
+  }
+  if (found.length === 0) {
+    var notFound = `No book found for search criteria`;
+    return notFound;
+  }
+  return found;
+}
 
-module.exports = { };
+module.exports = { issueCard, searchByAuthor };
