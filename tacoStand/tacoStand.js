417f566962e7d529bb06994b7cd459086272490d
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-17 21:39:02 -0400

    Pass tacoStand tests

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,35 @@
+function createIngredient(ingredient = "unknown", price = 0.0) {
+  var ingredient = {
+    name: ingredient,
+    price: price,
+  };
+  return ingredient;
+}
 
-module.exports = {}
+function createTaco(name = "custom", ingredients = []) {
+  var taco = {
+    name: name,
+    ingredients: ingredients,
+  };
+
+  return taco;
+}
+
+function addIngredientToTaco(taco, ingredient) {
+  taco.ingredients.push(ingredient);
+  return taco;
+}
+
+function calculatePrice(taco) {
+  var priceOfIngredients = 0;
+  for (let i = 0; i < taco.ingredients.length; i++) {
+    priceOfIngredients += taco.ingredients[i].price;
+  }
+  return priceOfIngredients;
+}
+module.exports = {
+  createIngredient,
+  createTaco,
+  addIngredientToTaco,
+  calculatePrice,
+};
