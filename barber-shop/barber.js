51d994563f26ce1e9986402351521d4cf693d78e
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-26 07:41:23 -0400

    Complete barber-shop challenges

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,32 @@
+function createBarber(name, earnings = 0, haircuts = []) {
+  var barber = {
+    name: name,
+    earnings: earnings,
+    haircuts: haircuts,
+  };
+  return barber;
+}
 
+function giveCompliment(haircut) {
+  return `This ${haircut.style} looks great!`;
+}
 
-module.exports = {  };
+function cutHair(barber, haircut) {
+  barber.haircuts.push(haircut);
+  barber.earnings += haircut.price;
+  return barber;
+}
+
+function listStyles(barber, hairLength) {
+  // return array of styles matching
+  // match styles and put them i an array
+  var styleMatch = [];
+  for (let i = 0; i < barber.haircuts.length; i++) {
+    if (barber.haircuts[i].hairLength === hairLength) {
+      styleMatch.push(barber.haircuts[i].style);
+    }
+  }
+  return styleMatch;
+}
+
+module.exports = { createBarber, giveCompliment, cutHair, listStyles };
