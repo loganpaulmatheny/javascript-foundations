45fe88db479ee9d287dd1752cea047c8ef77ca2e
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-26 07:57:57 -0400

    Complete meal planning challenge 🍳

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,27 @@
+function createMeal(mealName, calories, dishes = []) {
+  var meal = {
+    type: mealName,
+    calorieGoal: calories,
+    dishes: dishes,
+  };
+  return meal;
+}
 
-module.exports = { }
+function addDish(meal, dish) {
+  if (dish.calories < meal.calorieGoal) {
+    meal.dishes.push(dish);
+    meal.calorieGoal = meal.calorieGoal - dish.calories;
+  }
+  return meal;
+}
+
+function calculateCalories(meal) {
+  // return string template literal
+  // add the total calories for the meal
+  var totalCalories = 0;
+  for (let i = 0; i < meal.dishes.length; i++) {
+    totalCalories += meal.dishes[i].calories;
+  }
+  return `${meal.type} has a total of ${totalCalories} calories.`;
+}
+module.exports = { createMeal, addDish, calculateCalories };
